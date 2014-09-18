4e1e237d76abb5c5fe621757d1564fcac70e12e0
Andrew Bruce <andrew@camelpunch.com>
2014-09-18 19:48:42 +0100

    Passing tests, with logging enabled.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -2,6 +2,7 @@ import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 import static java.util.UUID.randomUUID 
 import groovy.json.JsonSlurper
+import java.util.logging.Logger
 
 db = [
   "match_requests": [],
@@ -9,59 +10,18 @@ db = [
   "results": []
 ]
 
-def played_match_ids(db) {
-  db["results"].collect { it["match_id"] }
-}
-
-def find_unplayed_match(db, match_request) {
-  db["matches"].
-    findAll { !played_match_ids(db).contains(it["id"]) }.
-    find {
-      it["player_1"] == match_request["requester_id"] ||
-        it["player_2"] == match_request["requester_id"] 
-    }
-}
-
-def results_involving_player(db, player_id) {
-  db["results"].findAll {
-    [it["winner"], it["loser"]].contains(player_id)
-  }
-}
-
-def previous_opponents(db, player_id) {
-  results_involving_player(db, player_id).collect {
-    it["winner"] == player_id ? it["loser"] : it["winner"]
-  }
-}
-
-def unfulfilled_match_requests(db) {
-  db["match_requests"].findAll { match_request ->
-    !db["matches"].any { match ->
-      match["match_request_1_id"] == match_request["id"] ||
-        match["match_request_2_id"] == match_request["id"]
-    }
-  }
-}
-
-def first_open_match_request(db, player_id) {
-  unfulfilled_match_requests(db).find {
-    inappropriate_opponents = [player_id] + previous_opponents(db, player_id)
-    !inappropriate_opponents.contains(it["requester_id"])
-  }
-}
-
-def record_match(db, open_match_request, new_match_request) {
-  db["matches"] << [
-    id: randomUUID(),
-    match_request_1_id: open_match_request["id"],
-    match_request_2_id: new_match_request["id"],
-    player_1: open_match_request["requester_id"],
-    player_2: new_match_request["requester_id"]
-  ]
-}
-
 ratpack {
+  def logger = Logger.getLogger("")
+
   handlers {
+    handler {
+      logger.info("" + request.method + " " + request.uri)
+      next()
+      def builder = new groovy.json.JsonBuilder()
+      builder db
+      logger.info("${builder.toPrettyString()}\n\n\n")
+    }
+
     delete("all") {
       db["match_requests"].clear()
       db["matches"].clear()
@@ -97,8 +57,9 @@ ratpack {
 
             if (found_match) {
               def builder = new groovy.json.JsonBuilder()
-                builder(match_id: found_match["id"])
-                response.send(builder.toString())
+              builder(match_id: found_match["id"], played_match_ids: played_match_ids(db),
+                      contained: played_match_ids(db).contains(found_match["id"]))
+              response.send(builder.toString())
             } else {
               clientError(404)
             }
@@ -121,5 +82,57 @@ ratpack {
     }
 
     assets "public"
+  }
+}
+
+def record_match(db, open_match_request, new_match_request) {
+  db["matches"] << [
+    id: randomUUID().toString(),
+    match_request_1_id: open_match_request["id"],
+    match_request_2_id: new_match_request["id"],
+    player_1: open_match_request["requester_id"],
+    player_2: new_match_request["requester_id"]
+  ]
+}
+
+def find_unplayed_match(db, match_request) {
+  def requester_id = match_request["requester_id"]
+
+  db["matches"].
+    find { match ->
+      !played_match_ids(db).contains(match.id) &&
+        (match.player_1 == requester_id || match.player_2 == requester_id)
+    }
+}
+
+def played_match_ids(db) {
+  db["results"].collect { it["match_id"] }
+}
+
+def first_open_match_request(db, player_id) {
+  unfulfilled_match_requests(db).find {
+    inappropriate_opponents = [player_id] + previous_opponents(db, player_id)
+    !inappropriate_opponents.contains(it["requester_id"])
+  }
+}
+
+def unfulfilled_match_requests(db) {
+  db["match_requests"].findAll { match_request ->
+    !db["matches"].any { match ->
+      match["match_request_1_id"] == match_request["id"] ||
+        match["match_request_2_id"] == match_request["id"]
+    }
+  }
+}
+
+def previous_opponents(db, player_id) {
+  results_involving_player(db, player_id).collect {
+    it["winner"] == player_id ? it["loser"] : it["winner"]
+  }
+}
+
+def results_involving_player(db, player_id) {
+  db["results"].findAll {
+    [it["winner"], it["loser"]].contains(player_id)
   }
 }
