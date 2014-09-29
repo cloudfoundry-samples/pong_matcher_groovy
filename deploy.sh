703e634ddaa53fea9ad584308e86f8c765bbfcf1
Andrew Bruce <andrew@camelpunch.com>
2014-09-29 10:37:24 +0100

    Dockerized deploys.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cd app
-./gradlew distZip
-cf api https://api.run.pivotal.io
-cf auth $CF_USERNAME $CF_PASSWORD
-cf target -o $CF_ORG -s $CF_SPACE
-cf push -n $HOSTNAME
+set -xe
+
+docker run -t camelpunch/pong-matcher-groovy /bin/bash -c "\
+    cd pong_matcher_groovy
+    ./gradlew distZip &&
+    cf api https://api.run.pivotal.io &&
+    cf auth $CF_USERNAME $CF_PASSWORD &&
+    cf target -o $CF_ORG -s $CF_SPACE &&
+    cf push -n $HOSTNAME -d cfapps.io"
