d69ca33960da80c5b6b2462d1f6d66a5d80f0ccd
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 13:20:46 +0000

    Attempt to pull image before building

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -ex
+
+docker pull docker.gocd.cf-app.com:5000/pong-matcher-groovy || true
 docker build -t docker.gocd.cf-app.com:5000/pong-matcher-groovy .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-groovy
