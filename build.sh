86d8836ffd73e7fb697689f47ba6a838cea082e3
Andrew Bruce <andrew@camelpunch.com>
2014-11-17 13:54:03 +0000

    Move GoCD stuff into pong_matcher_gocd_scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-docker pull docker.gocd.cf-app.com:5000/pong-matcher-groovy || true
-docker build $DOCKER_BUILD_OPTS -t docker.gocd.cf-app.com:5000/pong-matcher-groovy .
-docker push docker.gocd.cf-app.com:5000/pong-matcher-groovy
